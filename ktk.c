7bc949f89a92f9e2f401ed4fa8ff8ec7050dc3ed
Pham Dang Trinh <trinh.pham-dang@internal.banvien.com>
2021-01-15 11:05:01 +0700

    verify ktk_C 2nd

--- ktk.c
+++ ktk.c
@@ -4,4 +4,13 @@ test5----------
 main: add----------
 
 
-verify 1501--------
+verify 1501--------1st
+dsdff
+dsdfd
+------------------
+
+
+verify 1501--------2nd
+dfjkfkdjfd
+dfsdf
+-------------------
